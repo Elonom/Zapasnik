78f9341bd9e61d4278898296d3fa37c8d4ab7fc9
Mateusz Wesołowski <emelwesolowski@gmail.com>
2020-11-04 11:29:22 +0100

    First commit

---
+++ src/main/java/com/example/Zapasnik/ZapasnikApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zapasnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapasnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapasnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Zapasnik/ZapasnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zapasnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapasnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
